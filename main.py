abec51210eab60bdf2030c78079b590210bbed6f
Ellas25 <97697077+Ellas25@users.noreply.github.com>
2022-04-28 14:34:41 +0100

    updated to fill the computer screen

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import dice
 
 
 background = (250, 250, 250)#at the moment I am setting this to 250,250,250 untill I can get the window to open
-width = (500) # width may change to fill the whole of the screen
-height = (400) # height may change to fill the whole of the screen
+width = (1500) # width may change to fill the whole of the screen
+height = (1400) # height may change to fill the whole of the screen
 screen = pygame. display. set_mode((width, height))
 screen.fill(background)
 pygame.display.flip()
